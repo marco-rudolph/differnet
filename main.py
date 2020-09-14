16d69d6d2a27a2902b87a6ea5816eeb1f4508164
Shi Jin <jinzishuai@gmail.com>
2020-09-14 13:39:12 -0600

    train without test: save model and weight

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ from utils import load_datasets, make_dataloaders
 
 train_set, test_set = load_datasets(c.dataset_path, c.class_name)
 train_loader, test_loader = make_dataloaders(train_set, test_set)
-model = train(train_loader, test_loader)
+model = train(train_loader, None)
