a43d71f684c3979a8e276a8b74628cc2959a9eae
kuangzijian <kuangzijian1@hotmail.com>
2020-11-09 19:17:25 -0700

    Revert back the Utils, and remove the part that apply the concept of ResNet and train on the differences between samples and the averaged sample. #158

--- utils.py
+++ utils.py
@@ -20,24 +20,32 @@ def get_loss(z, jac):
 def load_datasets(dataset_path, class_name, test=False):
     '''
     Expected folder/file format to find anomalies of class <class_name> from dataset location <dataset_path>:
+
     train data:
+
             dataset_path/class_name/train/good/any_filename.png
             dataset_path/class_name/train/good/another_filename.tif
             dataset_path/class_name/train/good/xyz.png
             [...]
+
     test data:
+
         'normal data' = non-anomalies
+
             dataset_path/class_name/test/good/name_the_file_as_you_like_as_long_as_there_is_an_image_extension.webp
             dataset_path/class_name/test/good/did_you_know_the_image_extension_webp?.png
             dataset_path/class_name/test/good/did_you_know_that_filenames_may_contain_question_marks????.png
             dataset_path/class_name/test/good/dont_know_how_it_is_with_windows.png
             dataset_path/class_name/test/good/just_dont_use_windows_for_this.png
             [...]
+
         anomalies - assume there are anomaly classes 'crack' and 'curved'
+
             dataset_path/class_name/test/crack/dat_crack_damn.png
             dataset_path/class_name/test/crack/let_it_crack.png
             dataset_path/class_name/test/crack/writing_docs_is_fun.png
             [...]
+
             dataset_path/class_name/test/curved/wont_make_a_difference_if_you_put_all_anomalies_in_one_class.png
             dataset_path/class_name/test/curved/but_this_code_is_practicable_for_the_mvtec_dataset.png
             [...]
